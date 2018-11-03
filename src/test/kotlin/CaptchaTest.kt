be08236c76d1e582c527cce5cc577910958f7c12
minatorak <hotmail22866@gmail.com>
2018-11-03 21:10:01 +0700

    refactor test

--- src/test/kotlin/CaptchaTest.kt
+++ src/test/kotlin/CaptchaTest.kt
@@ -2,21 +2,24 @@ import org.junit.Assert
 import org.junit.Test
 
 class CaptchaTest {
+    private val operatorDummy = 1
+    private val rightOperandDummy = 1
+
     @Test
     fun firstPatternLeftShouldBeReturnOne() {
-        val captcha = Captcha(1,1,1,1)
+        val captcha = Captcha(1,1, operatorDummy, rightOperandDummy)
         Assert.assertEquals("One",captcha.getLeftOperand())
     }
 
     @Test
     fun firstPatternLeftShouldBeReturnFive() {
-        val captcha = Captcha(1,5,1,1)
+        val captcha = Captcha(1,5,operatorDummy,rightOperandDummy)
         Assert.assertEquals("Five",captcha.getLeftOperand())
     }
 
     @Test
     fun firstPatternLeftShouldBeReturnNine() {
-        val captcha = Captcha(1,9,1,1)
+        val captcha = Captcha(1,9,operatorDummy,rightOperandDummy)
         Assert.assertEquals("Nine",captcha.getLeftOperand())
     }
 }
