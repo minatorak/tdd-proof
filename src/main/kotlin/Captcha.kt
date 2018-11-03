6e5e8f7af44d3c08ae7d0f87096b61522d59b984
minatorak <hotmail22866@gmail.com>
2018-11-03 21:18:28 +0700

    refactor code Captha

--- src/main/kotlin/Captcha.kt
+++ src/main/kotlin/Captcha.kt
@@ -1,9 +1,8 @@
-class Captcha(private val pattern: Int, private val LeftOperand: Int,private val Operator: Int,private val RightOperand: Int) {
+class Captcha(private val pattern: Int, private val leftOperand: Int, private val operator: Int, private val rightOperand: Int) {
 
     fun getLeftOperand(): String {
-        if (LeftOperand == 9) return "Nine"
-        if (LeftOperand == 5) return "Five"
-        return "One"
+        val stringOperand: List<String> = listOf("One", "Two", "Three", "Four", "Five", "Six", "Seven","Eight", "Nine")
+        return stringOperand[leftOperand - 1]
     }
 
 }
