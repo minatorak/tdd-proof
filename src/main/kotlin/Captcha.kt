eb981a0046ba22750e9502138dd69026f90aa7a9
minatorak <hotmail22866@gmail.com>
2018-11-03 21:07:47 +0700

    first pattern input 9 return Nine

--- src/main/kotlin/Captcha.kt
+++ src/main/kotlin/Captcha.kt
@@ -1,6 +1,7 @@
 class Captcha(private val pattern: Int, private val LeftOperand: Int,private val Operator: Int,private val RightOperand: Int) {
 
-    fun getLeft(): String {
+    fun getLeftOperand(): String {
+        if (LeftOperand == 9) return "Nine"
         if (LeftOperand == 5) return "Five"
         return "One"
     }
--- src/test/kotlin/CaptchaTest.kt
+++ src/test/kotlin/CaptchaTest.kt
@@ -5,14 +5,18 @@ class CaptchaTest {
     @Test
     fun firstPatternLeftShouldBeReturnOne() {
         val captcha = Captcha(1,1,1,1)
-        Assert.assertEquals("One",captcha.getLeft())
+        Assert.assertEquals("One",captcha.getLeftOperand())
     }
 
     @Test
     fun firstPatternLeftShouldBeReturnFive() {
         val captcha = Captcha(1,5,1,1)
-        Assert.assertEquals("Five",captcha.getLeft())
+        Assert.assertEquals("Five",captcha.getLeftOperand())
     }
 
-
+    @Test
+    fun firstPatternLeftShouldBeReturnNine() {
+        val captcha = Captcha(1,9,1,1)
+        Assert.assertEquals("Nine",captcha.getLeftOperand())
+    }
 }
