b810b4c3abf619882617c8302607c6dba06e6135
minatorak <hotmail22866@gmail.com>
2018-11-03 23:36:05 +0700

    refactor class Captcha

--- src/main/kotlin/Captcha.kt
+++ src/main/kotlin/Captcha.kt
@@ -1,22 +1,21 @@
 class Captcha(private val pattern: Int, private val leftOperand: Int, private val operator: Int, private val rightOperand: Int) {
 
     private val stringOperand: List<String> = listOf("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")
+    private val operators = listOf("+", "*", "-")
+    private val outScope = "Not support"
 
-    fun getLeftOperand(): String {
-        if (pattern == 2) return leftOperand.toString()
-        return stringOperand[leftOperand - 1]
+    fun getLeftOperand(): String = when (pattern) {
+        1 -> stringOperand[leftOperand - 1]
+        2 -> leftOperand.toString()
+        else -> outScope
     }
 
-    fun getOperator(): String {
-        val operators = listOf("+", "*", "-")
-        return operators[operator - 1]
-    }
+    fun getOperator(): String = operators[operator - 1]
 
-    fun getRightOperand(): String {
-        if (pattern == 2) {
-            return stringOperand[rightOperand - 1]
-        }
-        return rightOperand.toString()
+    fun getRightOperand(): String = when (pattern) {
+        1 -> rightOperand.toString()
+        2 -> stringOperand[rightOperand - 1]
+        else -> outScope
     }
 
     override fun toString(): String {
